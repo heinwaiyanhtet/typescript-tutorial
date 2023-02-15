53afee363e9e8150257e6382fdb70a35ea4c53c0
Hein Wai Yan Htet <heinwaiyanhtet@pop-os.localdomain>
2023-02-15 15:08:38 +0630

    start

---
+++ 01intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Hein Wai Yan Htet", age: 22 };
+console.log("Hello! Hein Wai Yan Htet");
+console.log(user.email);
---
+++ 01intro/intro.ts
@@ -0,0 +1,7 @@
+let user = {name:"Hein Wai Yan Htet",age:22}
+
+console.log("Hello! Hein Wai Yan Htet")
+console.log(user.email)
+
+
+
